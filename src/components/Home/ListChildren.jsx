c5a37ca443a151381393d0a30de440e1b3305a2e
Juan Mosquera <maxiplux@gmail.com>
2020-02-18 22:48:25 -0500

    Add : show children of current user

--- src/components/Home/ListChildren.jsx
+++ src/components/Home/ListChildren.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import CheckingServices from "../../services/checking/CheckingServices";
+import ParentServices from "../../services/parents/ParentServices";
+import { List, Avatar,PageHeader  } from 'antd';
 
 export default function ListChildren(props){
-    return <h2>List</h2>;
+    const [dataSource, setDataSource] = useState([]);
+
+    useEffect(() => {
+        if (props.user) {
+            ParentServices.getChildren(props.user).then(response => {
+                setDataSource(response.data.map(  value=> { return  {title: value.FirstName, description:`School : ${value.school_name.name}`} } ));
+            });
+        }
+    }, [props.user]);
+    const fullName= `${props.user.user.FirstName} ${props.user.user.LastName}`;
+// documentarion from : https://ant.design/components/page-header/
+    return (
+
+        <>
+            <PageHeader
+                style={{
+                    border: '1px solid rgb(235, 237, 240)',
+                }}
+
+                title="Children from"
+                subTitle={fullName}
+            />
+        <List
+            itemLayout="horizontal"
+            dataSource={dataSource}
+            renderItem={item => (
+                <List.Item>
+                    <List.Item.Meta
+                        avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                        title={item.title}
+                        description={item.description}
+                    />
+                </List.Item>
+            )}></List>
+            </>
+
+    );
 
 }
